c41f27eb22f19a1fe51db2807353ce821a261ecd
Shubhankar Banerjee <shubhankarbanerjee3@gmail.com>
2026-08-16 16:26:44 +0530

    This is a new commit

---
+++ src/javaPracticeQuestions/f2_git.java
@@ -0,0 +1,12 @@
+package javaPracticeQuestions;
+
+public class f2_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is F2");
+
+	}
+
+}
